eb6f1d00ec0f897dc5a2708c5c9ea26a04d9d30a
toni14nexe <toni14nexe@gmail.com>
2022-10-05 12:05:45 +0200

    Added Axios notification for Online JSON api.

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 window.axios = axios
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://localhost:3000/'
+axios.defaults.baseURL = 'http://localhost:3000/' //online https://api.npoint.io/4b074a9452c139835840/ | local http://localhost:3000/
 axios.interceptors.request.use(function(config){
     config.headers.common = {
         Authorization: `Bearer ${localStorage.getItem('authtoken')}`,
